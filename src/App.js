59c998ded9728c014ae9c347f54c7d70b95f8506
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2023-07-02 22:40:36 +0800

    feat: Implement main App UI

--- src/App.js
+++ src/App.js
@@ -5,6 +5,25 @@ import mockData from './MOCK_DATA.json';
 function App() {
   return (
     <div className="App">
+      <div className="header">
+        <h2>News Articles</h2>
+      </div>
+      <div className="actions">
+        <div className="actions-left">
+          <div className="checkbox-container">
+            <input type="checkbox" />
+          </div>
+          <button className="publish">Publish</button>
+          <button className="delete">
+            Delete
+          </button>
+        </div>
+        <div className="actions-right">
+          <input type="text" placeholder="Search ..." />
+        </div>
+      </div>
+      <div className="card-container">
+      </div>
     </div>
   );
 }
