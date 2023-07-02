ef4e69478aee5aa28b64aa254ed5a6314531c9b2
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2023-07-02 23:12:26 +0800

    feat: Implement card section UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGripVertical, faEye, faCalendar, faUser } from '@fortawesome/free-solid-svg-icons';
 import mockData from './MOCK_DATA.json';
 
 function App() {
@@ -23,6 +25,49 @@ function App() {
         </div>
       </div>
       <div className="card-container">
+        {mockData.map((news) => (
+          <div className="card" key={news.id}>
+            <div className="left-section">
+              <div className="checkbox-container">
+                <span className="drag-icon">
+                  <FontAwesomeIcon icon={faGripVertical} />
+                </span>
+                <input
+                  type="checkbox"
+                />
+              </div>
+            </div>
+            <div className="middle-section">
+              <div className="top-section">
+                <h3>{news.title}</h3>
+              </div>
+              <div className="center-section">
+                <span>
+                  <FontAwesomeIcon icon={faUser} />
+                  {news.author}
+                </span>
+                <span>
+                  <FontAwesomeIcon icon={faCalendar} />
+                  {news.date}
+                </span>
+              </div>
+              <div className="bottom-section">
+                <p>{news.content}</p>
+                <button className="read-full-button">
+                  <FontAwesomeIcon icon={faEye} />
+                  Read Full
+                </button>
+              </div>
+            </div>
+            <div className="right-section">
+              <div className="center-section">
+                <span className="tag">#Sports</span>
+                <span className="tag">#Worldwide</span>
+                <span className="tag">#Local</span>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
